8dee8bae1febc7b67b92bd85dfdbaf9e31629f92
mustafa11647 <mustafatime79@gmail.com>
2025-03-24 21:21:25 +0300

    Add files via upload

---
+++ a2.py
@@ -0,0 +1,9 @@
+def onTime(n):
+    iteration=0
+    for i in range(1,n+1):
+        iteration+=1
+    print("when n is ",n,"iterations=",iteration)
+onTime(10)
+onTime(20)
+onTime(42)
+print("\nwith every'n'the time taken and iterations will increase")
